b5bf14039838f6753bdd9fcce82e10508446a56a
thetntm <michael.becker.123@gmail.com>
2020-02-03 21:44:36 -0800

    added an error message

--- NYTSearchJS.js
+++ NYTSearchJS.js
@@ -8,19 +8,32 @@ var endYear_JQ = $("#endYear")
 var searchButton_JQ = $("#searchButton")
 var clearButton_JQ = $("#clearButton")
 var resultsDiv_JQ = $("#results")
+var errorMessage_JQ = $("#errorMessage")
 //Functions
 
 //Event Functions
 function searchButtonClicked(event)
 {
+    const numberOfItems = limit_JQ.val();
     const query = searchTerm_JQ.val();
+
+    if(!query || !(parseInt(numberOfItems)))
+    {
+        errorMessage_JQ.text("You did not enter the neccesary information!");
+        return;
+    }
+
+    const startYear = startYear_JQ.val();
+    const endYear = endYear_JQ.val();
+
+
     const queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + query + '&api-key=7pKBOYLgmdVv44wOY7x81sNfB88FjFjW'
     let requestPromise = $.ajax({
         url: queryURL,
         method: "GET",
         success: function(response) 
         {
-            console.log(response);
+
         }
       });
 }
