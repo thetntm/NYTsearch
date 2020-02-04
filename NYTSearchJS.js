49616962824a950c399763e5f6aba7ded10df839
thetntm <michael.becker.123@gmail.com>
2020-02-03 21:14:16 -0800

    added dom element variables

---
+++ NYTSearchJS.js
@@ -0,0 +1,17 @@
+//Declaring Variables
+
+//Dom elements
+var searchTerm_JQ = $("#searchTerm")
+var limit_JQ = $("#limit")
+var startYear_JQ = $("#startYear")
+var endYear_JQ = $("#endYear")
+var searchButton_JQ = $("#searchButton")
+var clearButton_JQ = $("#clearButton")
+var resultsDiv_JQ = $("#results")
+//Functions
+
+
+
+//Event assignment
+
+//Code to run on startup
